381c910c158f20a02ebcaca205b5fcff815723be
oliver-goeken <oliverdgoeken@gmail.com>
2024-04-21 11:44:11 -0700

    update install script

--- install.sh
+++ install.sh
@@ -1,9 +1,29 @@
 #!/usr/bin/env bash
 
-touch ~/.vimrc
-VIMRCDIFF=$(diff ~/.vimrc config/vimrc)
-if [ "$VIMRCDIFF" != "" ]
-then
-	echo "~/.vimrc not found, installing... "
-	sudo ln -s configure/vimrc ~/.vimrc
-fi
+create_symlink () {
+	sudo ln -s config/$1 ~/.$1
+}
+
+for filename in vimrc
+do
+	if [ -e ~/.$filename  ]
+	then
+		if [ -L ~/.$filename ]
+		then
+			if ! [ "~/.$filename" -ef "config/$filename" ]
+			then
+				echo "incorrect $filename symlink found, updating..."
+				unlink ~/.$filename
+				create_symlink
+			fi
+		else
+			echo "non-linked $filename found, creating backup at..."
+			mv ~/.$filename ~/.$filename.old
+			echo "creating new symlink..."
+			create_symlink
+		fi
+	else
+		echo "$filename not found, creating symlink..."
+		create_symlink
+	fi
+done
