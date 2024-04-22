d1c2c5695e0660ddbffd93d6870ce31fe4084f10
oliver-goeken <oliverdgoeken@gmail.com>
2024-04-22 09:52:24 -0700

    fix zshrc location finding

--- install.sh
+++ install.sh
@@ -72,16 +72,14 @@ then
 	print_if_verbose $(echo /tmp/dotfile_clone)
 fi
 
-ZSH_START_FILE=/etc/zsh/zshenv
-if [[ "$OSTYPE" == "darwin"* ]]; then
-	ZSH_START_FILE=/etc/zshenv
-fi
-
-if ! grep -F -x -q 'eval "$(dircolors ~/.dir_colors)"' $ZSH_START_FILE > /dev/null
-then
-	echo "adding ls colors..."
-	dircolors --print-database > ~/.dir_colors
-	echo 'eval "$(dircolors ~/.dir_colors)"' | sudo tee -a $ZSH_START_FILE > /dev/null
+if ! [[ "$OSTYPE" == "darwin"* ]]; then
+	ZSH_START_FILE=/etc/zsh/zshenv
+	if ! grep -F -x -q 'eval "$(dircolors ~/.dir_colors)"' $ZSH_START_FILE > /dev/null
+	then
+		echo "adding ls colors..."
+		dircolors --print-database > ~/.dir_colors
+		echo 'eval "$(dircolors ~/.dir_colors)"' | sudo tee -a $ZSH_START_FILE > /dev/null
+	fi
 fi
 
 
