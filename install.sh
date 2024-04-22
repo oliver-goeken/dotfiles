b415eb5139f660b8233ddb59f75dbdbc649ad027
oliver-goeken <oliverdgoeken@gmail.com>
2024-04-22 00:17:45 -0700

    add ls color support

--- install.sh
+++ install.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 
+VERBOSE=0
 if [ $# -eq 1 ]
 then
 	if [ $1 = '--help' ] || [ $1 = '-h' ]
@@ -8,7 +9,6 @@ then
 		exit 0
 	fi
 
-	VERBOSE=0
 	if [ $1 = '--verbose' ] || [ $1 = '-v' ]
 	then
 		VERBOSE=1
@@ -41,7 +41,7 @@ do
 				unlink ~/.$filename
 				create_symlink $filename
 			else
-				echo "$filename symlink already exists!"
+				print_if_verbose "$filename symlink already exists!"
 			fi
 		else
 			echo "non-linked $filename found, creating backup at \"~/.$filename.old\"..."
@@ -67,15 +67,18 @@ if ! [ -d $DOTFILEDIR/plugins/zsh/zsh-syntax-highlighting ]
 then
 	echo "cloning zsh-syntax-highlighting..."
 	cd $DOTFILEDIR/plugins/zsh
-	git clone https://github.com/zsh-users/zsh-syntax-highlighting.git >> /tmp/dotfile_clone
+	git clone https://github.com/zsh-users/zsh-syntax-highlighting.git > /tmp/dotfile_clone
 	echo /tmp/dotfile_clone >> ~/.dotfiles.log
 	print_if_verbose $(echo /tmp/dotfile_clone)
 fi
 
-if ! grep -Fxq "eval \"$(dircolors ~/.dir_colors)\"" /etc/zshrc
+ZSH_START_FILE=/etc/zsh/zshenv
+
+if ! grep -F -x -q 'eval "$(dircolors ~/.dir_colors)"' $ZSH_START_FILE > /dev/null
 then
+	echo "adding ls colors..."
 	dircolors --print-database > ~/.dir_colors
-	echo "eval \"$(dircolors ~/.dir_colors)\"" | sudo tee -a /etc/zshrc > /dev/null
+	echo 'eval "$(dircolors ~/.dir_colors)"' | sudo tee -a $ZSH_START_FILE > /dev/null
 fi
 
 
